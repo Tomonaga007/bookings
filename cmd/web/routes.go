adbd162d9b19a50e3664d0913e2fe72b4c762790
dominik <swierzk@gmail.com>
2021-09-13 21:57:24 +0200

    Convert html-source files to golang template files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,11 @@ func routes(app *config.AppConfig) http.Handler{
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/apartment-1", http.HandlerFunc(handlers.Repo.Apartment_1))
+	mux.Get("/apartment-2", http.HandlerFunc(handlers.Repo.Apartment_2))
+	mux.Get("/search-availability", http.HandlerFunc(handlers.Repo.Availability))
+	mux.Get("/contact", http.HandlerFunc(handlers.Repo.Contact))
+	mux.Get("/make-reservation", http.HandlerFunc(handlers.Repo.Reservation))
 
 	fileSrever := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileSrever))
