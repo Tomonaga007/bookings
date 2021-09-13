777fa0babaf519759f838ed0e7f284b7317eb321
dominik <swierzk@gmail.com>
2021-09-13 22:20:28 +0200

    Install nosurf package and add middleware to protect against CSRF attacks

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/nosurf"
+	"net/http"
+
+	)
 
 //func WriteToConsole(next http.Handler) http.Handler {
 //	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +15,15 @@ import "net/http"
 
 func SessionLoad(next http.Handler) http.Handler{
 	return session.LoadAndSave(next)
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path: "/",
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,11 +5,14 @@ import (
 	"github.com/Tomonaga007/bookings/pkg/handlers"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+
 )
 
 func routes(app *config.AppConfig) http.Handler{
 	mux := chi.NewRouter()
 	mux.Use(SessionLoad)
+	mux.Use(NoSurf)
+
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
